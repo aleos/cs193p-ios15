a658dc980c440570c811f29a0090465cb26523a3
Alexander Ostrovsky <thealeos@gmail.com>
2024-09-22 18:04:03 +0400

    Add a title “Memorize!” to the top of the screen

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Text("Memorize!").font(.largeTitle)
             ScrollView {
                 cards
             }
