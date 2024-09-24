0370bd0de51c2fa7496261a3d30aa228285d6780
Alexander Ostrovsky <thealeos@gmail.com>
2024-09-24 09:46:47 +0400

    Get the Memorize game working as demonstrated in lecture

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -20,6 +20,7 @@ struct EmojiMemoryGameView: View {
             Text("Memorize!").font(.largeTitle)
             ScrollView {
                 cards
+                    .animation(.default, value: viewModel.cards)
             }
             Spacer()
             Button("Shuffle") {
@@ -32,10 +33,13 @@ struct EmojiMemoryGameView: View {
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(viewModel.cards[index])
+            ForEach(viewModel.cards) { card in
+                CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
                     .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
         }
         .foregroundStyle(currentTheme.foregroundColor)
@@ -84,9 +88,11 @@ struct CardView: View {
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0 : 1)
         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
 #Preview {
-    EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+    @Previewable @StateObject var viewModel = EmojiMemoryGame()
+    EmojiMemoryGameView(viewModel: viewModel)
 }
